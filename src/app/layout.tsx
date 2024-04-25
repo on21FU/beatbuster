63787d241d75a002a10932c4976f8dd500748489
benniho03 <ben.holderle.21@lehre.mosbach.dhbw.de>
2024-04-25 09:39:49 +0200

    added bootstrap and sass

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
-import "~/styles/globals.css";
+import "~/styles/globals.scss";
+
+import bootstrap from "bootstrap"
 
 import { Inter } from "next/font/google";
 
@@ -21,6 +23,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous"></link>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossOrigin="anonymous"></script>
       <body className={inter.className}>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
