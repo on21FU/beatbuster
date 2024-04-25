2591f5d91c92944ffcb5e5a906311135685a0ead
on21FU <94552370+on21FU@users.noreply.github.com>
2024-04-25 09:41:39 +0200

    set up clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "~/styles/globals.css";
-
 import { Inter } from "next/font/google";
-
+import { ClerkProvider } from '@clerk/nextjs'
 import { TRPCReactProvider } from "~/trpc/react";
 
 const inter = Inter({
@@ -17,13 +16,15 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
-      </body>
-    </html>
-  );
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
 }
