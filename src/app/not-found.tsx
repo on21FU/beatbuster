c01bb948e2b2966e0c4fe992b3d1b153e4a20bc6
paulhnn26 <pau.henn.21@lehre.mosbach.dhbw.de>
2024-05-15 11:27:45 +0200

    404 Page added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <div className='error-page container'>
+      <h2>404 Ooops!</h2>
+      <h3>Look like you are lost...</h3>
+      <p>The page you are looking for is not available</p>
+        <a href="/">Return Home</a>
+    </div>
+  )
+}
