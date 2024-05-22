de0111a9bb004c9384359d3ac4a80a15d0ce171a
on21FU <94552370+on21FU@users.noreply.github.com>
2024-05-22 14:16:37 +0200

    change clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,13 @@ import {
     createRouteMatcher
   } from '@clerk/nextjs/server';
    
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  '/game(.*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: [ '/((?!.*\\..*|_next).*)', '/(api|trpc)(.*)'],
