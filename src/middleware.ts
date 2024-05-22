4dd6c66b0bc23953f0f179b4c24f99c6fe76dab3
on21FU <94552370+on21FU@users.noreply.github.com>
2024-05-22 13:35:17 +0200

    change middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,8 @@ import {
     createRouteMatcher
   } from '@clerk/nextjs/server';
    
-  const isProtectedRoute = createRouteMatcher([
-    '/',
-  ]);
    
-  export default clerkMiddleware((auth, req) => {
-    if (isProtectedRoute(req)) auth().protect();
-  });
+  export default clerkMiddleware();
 
 export const config = {
   matcher: [ '/((?!.*\\..*|_next).*)', '/(api|trpc)(.*)'],
