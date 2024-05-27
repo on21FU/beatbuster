296303fe59a640a7c9f29d9704b4b9523f3dee05
on21FU <94552370+on21FU@users.noreply.github.com>
2024-05-27 09:39:17 +0200

    wip

--- src/app/game/[gameId]/page.tsx
+++ src/app/game/[gameId]/page.tsx
@@ -1,24 +1,40 @@
-"use server"
+"use server";
 import { auth, clerkClient } from "@clerk/nextjs/server";
 import { Game } from "./gameSetup";
 import GameConfigWrapper from "~/app/components/gameConfigWrapper";
 import { getUserToken } from "~/app/spotify";
 
-export default async function GamePage({ params }: { params: { gameId: string } }) {
-    const userId = auth().userId
-    if (!userId) return <div>Not logged in</div>
-    const { fullName, imageUrl, id } = await clerkClient.users.getUser(userId)
-    const { token: accessToken, error } = await getUserToken()
+export default async function GamePage({
+    params,
+}: {
+    params: { gameId: string };
+}) {
+    const userId = auth().userId;
+    if (!userId) return <div>Not logged in</div>;
+    const { fullName, imageUrl, id } = await clerkClient.users.getUser(userId);
+    const { token: accessToken, error } = await getUserToken();
 
-    return <Game error={JSON.stringify(error)} webSocketUrl={process.env.WEBSOCKET_URL ?? ""} accessToken={ accessToken} gameId={params.gameId} user={{
-        userId: id,
-        username: fullName || generateGuestName(),
-        imageUrl: imageUrl || "",
-    }} >
-        <GameConfigWrapper />
-    </Game>
+    return (
+        <Game
+            error={stringifyError(error)!.toString()}
+            webSocketUrl={process.env.WEBSOCKET_URL ?? ""}
+            accessToken={accessToken}
+            gameId={params.gameId}
+            user={{
+                userId: id,
+                username: fullName || generateGuestName(),
+                imageUrl: imageUrl || "",
+            }}
+        >
+            <GameConfigWrapper />
+        </Game>
+    );
 }
 
 function generateGuestName() {
-    return "Guest" + Math.floor(Math.random() * 1000)
+    return "Guest" + Math.floor(Math.random() * 1000);
+}
+
+function stringifyError(error: unknown) {
+    return typeof error === "string" ? error : undefined
 }
